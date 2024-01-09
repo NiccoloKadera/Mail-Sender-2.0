fd1821af0fac871a898de079ffea8e20cddb9128
NiccoloKadera <niccolokadera@gmail.com>
2024-01-09 11:05:01 +0100

    Bug fix
    Bug fix in HTML Mail generator

--- ENG/functions.py
+++ ENG/functions.py
@@ -162,7 +162,7 @@ class HTMLMailBuilder:
         <head>
             <meta http-equiv="Content-Type" content="text/html; charset-utf-8" />
             <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-            <meta name-"viewport" content="width=device-width, initial-scale=1.0">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>NKMailTemplate</title>
             <style type="text/css">
                 body {
